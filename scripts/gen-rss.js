c36b4f945478612cea4c293d760e1a061c110dd4
Corin Faife <corin.faife@gmail.com>
2023-07-16 12:54:32 -0400

    Edit RSS script

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Corin Faife',
+    site_url: 'https://corinfaife.co',
+    feed_url: 'https://corinfaife.co/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
