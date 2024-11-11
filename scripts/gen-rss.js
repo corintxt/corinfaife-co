7f42ab80fd683457a3a617ea350bb08e4715c164
Corin FAIFE <corin.faife@afp.com>
2024-11-11 16:00:54 -0500

    Add second function to gen-rss.js

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -3,7 +3,7 @@ const path = require('path')
 const RSS = require('rss')
 const matter = require('gray-matter')
 
-async function generate() {
+async function generate_blog() {
   const feed = new RSS({
     title: 'Blog | corinfaife.co',
     site_url: 'https://corinfaife.co',
@@ -39,4 +39,43 @@ async function generate() {
   await fs.writeFile('./public/feed.xml', feed.xml({ indent: true }))
 }
 
-generate()
+async function generate_bytes() {
+  const feed = new RSS({
+    title: 'Bytes | corinfaife.co',
+    site_url: 'https://corinfaife.co',
+    feed_url: 'https://corinfaife.co/bytes.xml'
+  })
+
+  const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'bytes'))
+  const allPosts = []
+  await Promise.all(
+    posts.map(async (name) => {
+      if (name.startsWith('index.')) return
+
+      const content = await fs.readFile(
+        path.join(__dirname, '..', 'pages', 'bytes', name)
+      )
+      const frontmatter = matter(content)
+
+      allPosts.push({
+        title: frontmatter.data.title,
+        url: '/bytes/' + name.replace(/\.mdx?/, ''),
+        date: frontmatter.data.date,
+        description: frontmatter.data.description,
+        categories: frontmatter.data.tag.split(', '),
+        author: frontmatter.data.author
+      })
+    })
+  )
+
+  allPosts.sort((a, b) => new Date(b.date) - new Date(a.date))
+  allPosts.forEach((post) => {
+      feed.item(post)
+  })
+  await fs.writeFile('./public/bytes.xml', feed.xml({ indent: true }))
+}
+
+generate_blog()
+generate_bytes()
+
+
