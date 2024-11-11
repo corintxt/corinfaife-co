e8e515f647d239676431b4c184e223bdabdc205d
Corin FAIFE <corin.faife@afp.com>
2024-11-11 15:45:48 -0500

    Modify Bytes RSS URL

--- scripts/gen-rss-bytes.js
+++ scripts/gen-rss-bytes.js
@@ -23,7 +23,7 @@ async function generate() {
 
       allPosts.push({
         title: frontmatter.data.title,
-        url: '/bytes/' + name.replace(/\.mdx?/, ''),
+        url: '/Bytes/' + name.replace(/\.mdx?/, ''),
         date: frontmatter.data.date,
         categories: frontmatter.data.tag.split(', '),
         author: frontmatter.data.author
